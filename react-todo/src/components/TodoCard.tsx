1febb8c452a10c237c2d634694cbf72f41b127ec
Arathi <arathibiju2000@gmail.com>
2021-12-26 23:06:10 +1300

    Fixed todo card formatting on the buttons

--- react-todo/src/components/TodoCard.tsx
+++ react-todo/src/components/TodoCard.tsx
@@ -5,11 +5,10 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
+  root: {},
   title: {
     fontSize: 14,
   },
@@ -23,28 +22,50 @@ export default function SimpleCard() {
 
   return (
     <Card className={classes.root}>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          Title
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Time added
-        </Typography>
-        <Typography variant="body2" component="p">
-          Text that is inside the todo.
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" aria-label="show more" color="primary">
-          View
-        </Button>
-        <Button size="small" aria-label="edit todo card" color="primary">
-          Edit
-        </Button>
-        <Button size="small" aria-label="delete" color="secondary">
-          Delete
-        </Button>
-      </CardActions>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid item>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              Title
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              Time added
+            </Typography>
+            <Typography variant="body2" component="p">
+              Text that is inside the todo.
+            </Typography>
+          </CardContent>
+        </Grid>
+        <CardActions>
+          <Grid
+            container
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="flex-end"
+          >
+            <Grid item>
+              <Button size="small" aria-label="show more" color="primary">
+                View
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button size="small" aria-label="edit todo card" color="primary">
+                Edit
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button size="small" aria-label="delete" color="secondary">
+                Delete
+              </Button>
+            </Grid>
+          </Grid>
+        </CardActions>
+      </Grid>
     </Card>
   );
 }
