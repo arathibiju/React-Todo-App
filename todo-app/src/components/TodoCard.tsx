0a75bcad9c61d448d92bbc59a08a6cd95a2ea233
Arathi Biju <arathi.biju@serko.com>
2022-02-09 14:21:33 +1300

    responsive grid

--- todo-app/src/components/TodoCard.tsx
+++ todo-app/src/components/TodoCard.tsx
@@ -4,10 +4,6 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Divider, Stack } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from "@mui/icons-material/Edit";
-import DoneOutlineIcon from "@mui/icons-material/DoneOutline";
 
 interface Props {
   todosTitle: string;
@@ -20,39 +16,22 @@ export const TodoCard = ({ todosTitle, todosBody, handleDelete }: Props) => {
     handleDelete(todosTitle, todosBody);
   };
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ maxWidth: 345 }}>
       <CardContent>
         <Typography variant="h5" component="div">
           {todosTitle}
         </Typography>
-        <Typography variant="body2">{todosBody}</Typography>
+        <Typography gutterBottom variant="body2" color="text.secondary">
+          {todosBody}
+        </Typography>
       </CardContent>
       <CardActions>
-        <Stack
-          direction="row"
-          divider={<Divider orientation="vertical" flexItem />}
-          spacing={2}
-        >
-          <Button
-            size="small"
-            variant="outlined"
-            startIcon={<DoneOutlineIcon />}
-          >
-            Mark Done
-          </Button>
-          <Button size="small" variant="outlined" startIcon={<EditIcon />}>
-            Edit
-          </Button>
-          <Button
-            size="small"
-            variant="outlined"
-            color="error"
-            startIcon={<DeleteIcon />}
-            onClick={onClick}
-          >
-            Delete
-          </Button>
-        </Stack>
+        <Button size="small" variant="outlined">
+          Edit
+        </Button>
+        <Button size="small" variant="outlined" color="error" onClick={onClick}>
+          Delete
+        </Button>
       </CardActions>
     </Card>
   );
--- todo-app/src/components/TodoList.tsx
+++ todo-app/src/components/TodoList.tsx
@@ -1,3 +1,4 @@
+import { Box, Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import React from "react";
 import { TodoCard } from "./TodoCard";
@@ -14,14 +15,22 @@ export const TodoList = ({ todosTitle, todosBody, handleDelete }: Props) => {
       Trying adding a task using the button on the app bar
     </Typography>
   ) : (
-    <div>
-      {todosTitle.map((t, index) => (
-        <TodoCard
-          todosTitle={t}
-          handleDelete={handleDelete}
-          todosBody={todosBody[index]}
-        />
-      ))}
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {todosTitle.map((t, index) => (
+          <Grid item xs={2} sm={4} md={4}>
+            <TodoCard
+              todosTitle={t}
+              handleDelete={handleDelete}
+              todosBody={todosBody[index]}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
